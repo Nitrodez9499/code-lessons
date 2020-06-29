2a77d3123ac58c973d0b10f26b4aa224cfe7e0ee
CourseraCoder <rzrrocket@gmail.com>
2020-06-29 18:01:46 -0400

    made new func and new log

--- javascript/lesson-alpha-javascript.js
+++ javascript/lesson-alpha-javascript.js
@@ -1,13 +1,34 @@
 // documentation HTMLInputElement: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
 
+// NOTES:
+    // everything is an object, for example:
+    // string is an object
+    // array is an object
+
+    // invoking a funciton means to call it
+    // for example: walkTheDog('maya')
+    // this invokes the function and passes it
+    // a string as an argument (or parameter)
+    // function walkTheDog(dogName) { //.. code here .. }
+
+    // parameter is an argument
+    // argument is a parameter
+    // they mean the same thing
+
 // MISSION:
 // 1. create a function that takes a parameter
 // 2. reverse string
 // 3. return string
 
-// TODAY: step 1, step 3
+// Yesterday: step 1, step 3
 
-// TOMOROOW: step2: reverse the string
+// TODAY: 
+    // create a function that takes 2 parameters
+    // put the parameters in an array
+    // return the array
+    // log the array from the calling function (hint: theBoxesHandler)
+
+// FUTURE: step2: reverse the string
 
 function theBoxesHandler(boxName) {
     console.log("The Iniewicz " + boxName);
@@ -17,19 +38,33 @@ function theBoxesHandler(boxName) {
     // TODAY
     // create variable to capture result (or return) of letterReverser
     // console.log( the return of letterReverser )
-    var captureWord = letterReverser('max');
+    var captureWord = letterReverser('chocolate ice cream');
+
     console.log(captureWord);
 
-
+    var getArray = doSomething("malgosia", "tomek")
+console.log(getArray);
     textBox.value = boxName;
 }
 
 // wordName is an argument or paremeter
 function letterReverser(wordName) {
 
+    let newWord = 'whipped cream and ' + wordName;
+
     // reverse the "wordName" argument (or paremeter)
-    return wordName;
+    return newWord;
     // put the reversed value in a variable
 
     // return the reversed value variable
 }
+function doSomething(max, xavy) {
+
+  var theIniewiczBros = [      
+      max,
+      xavy
+  ];
+
+  return theIniewiczBros;
+}
+
